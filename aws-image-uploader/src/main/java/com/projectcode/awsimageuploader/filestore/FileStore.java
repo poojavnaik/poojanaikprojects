ba134e4bb8c546651095c7c1350f0d9bf8c3bdce
poojavnaik <88692606+poojavnaik@users.noreply.github.com>
2021-08-16 15:34:57 +1000

    Update FileStore.java
    MetaData error updated

--- aws-image-uploader/src/main/java/com/projectcode/awsimageuploader/filestore/FileStore.java
+++ aws-image-uploader/src/main/java/com/projectcode/awsimageuploader/filestore/FileStore.java
@@ -2,7 +2,9 @@ package com.projectcode.awsimageuploader.filestore;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 
 import java.io.InputStream;
 import java.util.Map;
@@ -20,12 +22,19 @@ public class FileStore {
 	
 	//InputStream path goes and saves to Amazon S3 Bucket
 	public void save(String path, String fileName, Optional<Map<String, String>> optionalMetaData, InputStream input) {
-		
+		ObjectMetadata metadata = new ObjectMetadata();
+		optionalMetaData.ifPresent(map -> {
+			if (!map.isEmpty()) {
+				map.forEach(metadata::addUserMetadata);
+			}
+		});
 		try {
 			
-			s3.putObject(path, fileName, input, metadata)
+			s3.putObject(path, fileName, input, metadata);
 		}
-		
+		catch(AmazonServiceException e) {
+			throw new IllegalStateException("Failed to store file to S3", e);
+		}
 	}
 
 }
